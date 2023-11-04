0317fda9c59c2ba16ef87828abaff79767ac5123
Geoff Pleiss <824157+gpleiss@users.noreply.github.com>
2023-11-03 18:58:33 -0700

    Update schedule + announcements

--- src/pages/stat520p_index.js
+++ src/pages/stat520p_index.js
@@ -15,40 +15,18 @@ class Stat520PIndex extends React.Component {
           </SmallH2>
           <ul>
             <li>
-              The <Link href="../../stat520p_pset1.pdf">diagnostic problem set</Link> is
-              due on October 31.
-              (<Link href="../../stat520p_pset1.tex">TeX template</Link> and <Link href="../../math_commands.tex">math_commands.tex</Link> macros file.)
+              The coding exercise is due on Thursday, November 16.
+              (See Canvas for details.)
               Please message me if you need an extension.
             </li>
             <li>
-              Choose your <Link inside href="syllabus.html#final_assessment">final assessment option</Link> by
-              Tuesday, Oct 31. See Canvas announcement for instructions.
-              <ul>
-                <li>
-                  For those of you choosing the Topics Paper Presentation final assessment option,
-                  the list of potential papers <Link inside href="papers.html">has been posted</Link>.
-                  Choose a paper and add your name to the topics paper presentation spreadsheet (see Canvas).
-                </li>
-                <li>
-                  For those of you choosing the Research Project final assessment option,
-                  your <Link href="https://canvas.ubc.ca/courses/135093/assignments/1692854">
-                    1-page project proposal
-                  </Link> will be due on Tuesday, Nov 7.
-                </li>
-              </ul>
+              For those of you choosing the Research Project final assessment option,
+              your <Link href="https://canvas.ubc.ca/courses/135093/assignments/1692854">
+                1-page project proposal
+              </Link> will be due on Tuesday, Nov 7.
             </li>
             <li>
-              Demos of different Gaussian process covariance functions:
-              <ul>
-                <li>
-                  My <Link href="https://github.com/gpleiss/gp_bo_demos/blob/main/kernel_demo.ipynb">
-                    interactive Jupyter demo
-                  </Link> (download and play around with it locally)
-                </li>
-                <li>
-                  David Duvenaud's <Link href="https://www.cs.toronto.edu/~duvenaud/cookbook/">kernel cookbook</Link>
-                </li>
-              </ul>
+              The topics paper presentations are now posted.
             </li>
           </ul>
         </Section>
@@ -90,7 +68,12 @@ class Stat520PIndex extends React.Component {
 
               <tr>
                 <td>Class 3 (Tues, Oct 31)</td>
-                <td>Gaussian processes and kernels</td>
+                <td>
+                  Gaussian processes and kernels<br />
+                  (<Link href="https://github.com/gpleiss/gp_bo_demos/blob/main/kernel_demo.ipynb">
+                    Interactive Jupyter demo
+                   </Link>, David Duvenaud's <Link href="https://www.cs.toronto.edu/~duvenaud/cookbook/">kernel cookbook</Link>)
+                </td>
                 <td>{ textbook } Ch. 3, 9.1 (optional)</td>
                 <td>
                   <Link href="../../stat520p_pset1.pdf">Diagonstic problem set</Link>{ " " }
@@ -104,77 +87,113 @@ class Stat520PIndex extends React.Component {
               <tr>
                 <td>Class 4 (Thurs, Nov 2)</td>
                 <td>Decision theory and optimization theory</td>
-                <td>{ textbook } Ch. 5-6</td>
+                <td>{ textbook } Ch. 5</td>
                 <td></td>
               </tr>
 
               <tr>
                 <td>Class 5 (Tues, Nov 7)</td>
                 <td>Acquisition functions</td>
-                <td>{ textbook } Ch. 7</td>
+                <td>{ textbook } Ch. 6-7</td>
                 <td>Project proposal (for those choosing the project final asssessment option)</td>
               </tr>
 
               <tr>
                 <td>Class 6 (Thurs, Nov 9)</td>
                 <td>Implementation details</td>
-                <td>{ textbook } Ch. 8, 9.2</td>
+                <td></td>
                 <td></td>
               </tr>
 
               <tr>
                 <td>Class 7 (Tues, Nov 14)</td>
+                <td>Implementation details</td>
+                <td>{ textbook } Ch. 8, 9.2</td>
+                <td></td>
+              </tr>
+
+              <tr>
+                <td>Class 8 (Thurs, Nov 16)</td>
                 <td>Theory</td>
                 <td>{ textbook } Ch. 10</td>
                 <td>Short coding assignment</td>
               </tr>
 
               <tr>
-                <td>Class 8 (Thurs, Nov 16)</td>
-                <td>Summary and/or Special Topic (TBD)</td>
-                <td>TBD</td>
-                <td></td>
-              </tr>
-
-              <tr>
                 <td>Class 9 (Tues, Nov 21)</td>
-                <td>Special Topic (TBD)</td>
-                <td>TBD</td>
+                <td>
+                  Special Topic (Multi-Objective BO)
+                </td>
+                <td>
+                  <Link href="https://arxiv.org/abs/2109.10964">
+                    Multi-Objective Bayesian Optimization over High-Dimensional Search Spaces
+                  </Link>
+                </td>
                 <td></td>
               </tr>
 
               <tr>
                 <td>Class 10 (Thurs, Nov 23)</td>
-                <td>Special Topic (TBD)</td>
-                <td>TBD</td>
+                <td>
+                  Special Topic (Local BO)
+                </td>
+                <td>
+                  <Link href="https://arxiv.org/abs/1910.01739">
+                    Scalable Global Optimization via Local Bayesian
+                  </Link>
+                </td>
                 <td></td>
               </tr>
 
               <tr>
                 <td>Class 11 (Tues, Nov 28)</td>
-                <td>Special Topic (TBD)</td>
-                <td>TBD</td>
+                <td>
+                  Special Topic (Mixed-Space BO)
+                </td>
+                <td>
+                  <Link href="https://arxiv.org/abs/2210.10199">
+                    Bayesian Optimization over Discrete and Mixed Spaces via Probabilistic Reparameterization
+                  </Link>
+                </td>
                 <td></td>
               </tr>
 
               <tr>
                 <td>Class 12 (Thurs, Nov 30)</td>
-                <td>Special Topic (TBD)</td>
-                <td>TBD</td>
+                <td>
+                  Special Topic (Causal BO)
+                </td>
+                <td>
+                  <Link href="https://arxiv.org/abs/2005.11741">
+                    Causal Bayesian Optimization
+                  </Link>
+                </td>
                 <td></td>
               </tr>
 
               <tr>
                 <td>Class 13 (Tues, Dec 5)</td>
-                <td>Special Topic (TBD)</td>
-                <td>TBD</td>
+                <td>
+                  Special Topic (Preference BO)
+                </td>
+                <td>
+                  <Link href="https://arxiv.org/abs/1112.5745">
+                    Bayesian Active Learning for Classification and Preference Learning
+                  </Link>
+                </td>
                 <td></td>
               </tr>
 
               <tr>
                 <td>Class 14 (Thurs, Dec 7)</td>
-                <td>Special Topic (TBD)</td>
-                <td>TBD</td>
+                <td>
+                  Special Topic (Cost-Aware BO)
+                </td>
+                <td>
+                  <Link href="https://proceedings.mlr.press/v45/Xia15.html">
+                    Budgeted Bandit Problems with Continuous Random Costs
+                  </Link>
+                </td>
                 <td></td>
               </tr>
             </tbody>
