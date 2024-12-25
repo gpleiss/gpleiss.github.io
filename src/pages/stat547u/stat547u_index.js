11c7fc36fc942b11ef1d4238c94fd77c4f4efca4
Geoff Pleiss <824157+gpleiss@users.noreply.github.com>
2024-12-25 10:17:15 -0700

    Schedule

--- src/pages/stat547u/stat547u_index.js
+++ src/pages/stat547u/stat547u_index.js
@@ -1,20 +1,98 @@
 import React from 'react';
-//import { Table } from 'reactstrap';
+import { Table } from 'reactstrap';
 import {
-  //Link,
-  //SmallH2,
+  Link,
+  SmallH2,
   Section
 } from '../../components/utils.js';
+import classSchedule from './stat547u_schedule.json';
+
+
+const classDates = [
+  "07 Jan 2025",
+  "09 Jan 2025",
+  "14 Jan 2025",
+  "16 Jan 2025",
+  "21 Jan 2025",
+  "23 Jan 2025",
+  "28 Jan 2025",
+  "30 Jan 2025",
+  "04 Feb 2025",
+  "06 Feb 2025",
+  "11 Feb 2025",
+  "13 Feb 2025",
+];
+
+
+const assignments = {
+  "assignment1": (
+    <span>
+      <Link href="../../stat547u_pset1.pdf">Diagnostic problem set</Link> due on Tues, Jan 14.{ " " }
+      (
+        <Link href="../../stat547u_pset1.tex">TeX template</Link> and{ " " }
+        <Link href="../../math_commands.tex">math_commands.tex</Link> macros file.
+      )
+    </span>
+  
+  ),
+}
+
+
+function formatDate(inputDateStr) {
+  const date = new Date(Date.parse(inputDateStr));
+  // Define options for formatting the output
+  const options = { weekday: 'short', month: 'short', day: '2-digit' };
+  // Format the date to the desired string
+  return date.toLocaleDateString('en-US', options);
+}
 
 
 class Stat547UIndex extends React.Component {
   render() {
+    let index = 0;
+
     return (
-      <div className="col-12">
-        <Section>
-          <p>More info coming soon.</p>
+        <Section id="schedule" first>
+          <SmallH2>
+            Schedule
+          </SmallH2>
+          <p>
+            Subject to change; check <Link inside href="#announcements">announcements</Link> and Canvas!.
+          </p>
+          <Table striped hover>
+            <thead>
+              <tr>
+                <th>Class/Date</th>
+                <th>Class Topic</th>
+                <th>Reading Due</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {classSchedule.map((row) => (
+                (row.type === "class") ? (
+                  <tr>
+                    <td>Class {index += 1} ({formatDate(classDates[index - 1])})</td>
+                    <td>
+                      {row.topic}
+                    </td>
+                    <td>
+                      <Link href={row.readingUrl}>{row.readingTitle}</Link>
+                    </td>
+                  </tr>
+                ) : (row.type === "section") ? (
+                  <tr>
+                    <td colSpan="4" className="text-center">
+                      <em>{row.topic}</em>
+                      {(row.assignment) ? <br /> : (<span></span>)}
+                      {(row.assignment) ? (assignments[row.assignment]) : (<span></span>)}
+                    </td>
+                  </tr>
+                ) : (<div></div>)
+              ))}
+            </tbody>
+          </Table>
         </Section>
-      </div>
     );
   }
 }
