5c71e51159e9aa88b1cda237f92b6c44ed98b0bd
Geoff Pleiss <824157+gpleiss@users.noreply.github.com>
2025-01-05 18:30:12 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c3317c3341105e970fa358bea73583a5.js"
+  "/precache-manifest.07bfcf62315a1183432c6670b4214ec7.js"
 );
 
 self.addEventListener('message', (event) => {
