22333e2cab11c3a643d22fbe17d62ec649e18160
Geoff Pleiss <824157+gpleiss@users.noreply.github.com>
2025-01-06 16:39:24 -0800

    Lecture 01 notes

--- src/pages/stat547u/stat547u_lecture_notes.js
+++ src/pages/stat547u/stat547u_lecture_notes.js
@@ -1,5 +1,7 @@
+import introLecture from './lecture_notes/stat547u_lecture01.pdf';
 import rkhsLecture from './lecture_notes/stat547u_lecture02.pdf';
 
 export default {
+  "introLecture": introLecture,
   "rkhsLecture": rkhsLecture,
 };
