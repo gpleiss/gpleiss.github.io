8627a0ac8e7825314449c07b4b935ff390f4c4be
Geoff Pleiss <824157+gpleiss@users.noreply.github.com>
2025-01-15 09:05:46 -0800

    Update lecture notes

--- src/pages/stat547u/stat547u_lecture_notes.js
+++ src/pages/stat547u/stat547u_lecture_notes.js
@@ -1,9 +1,11 @@
 import introLecture from './lecture_notes/stat547u_lecture01.pdf';
 import rkhsLecture from './lecture_notes/stat547u_lecture02.pdf';
 import doubleDescentLecture from './lecture_notes/stat547u_lecture03.pdf';
+import rmtLecture from './lecture_notes/stat547u_lecture04.pdf';
 
 export default {
   "introLecture": introLecture,
   "rkhsLecture": rkhsLecture,
   "doubleDescentLecture": doubleDescentLecture,
+  "rmtLecture": rmtLecture,
 };
