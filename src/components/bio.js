f9511887adaa089b1fdc1eae9011111dc0de59f4
Geoff Pleiss <824157+gpleiss@users.noreply.github.com>
2026-04-22 15:12:30 -0700

    Add best student paper to bio

--- src/components/bio.js
+++ src/components/bio.js
@@ -4,6 +4,6 @@ as well as a Canada CIFAR AI Chair affiliated with the Vector Institute.
 He earned a Ph.D. in Computer Science from Cornell University under the supervision of Kilian Weinberger.
 Geoff specializes in uncertainty quantification in machine learning,
 especially within the contexts of Bayesian optimization, spatiotemporal modelling, and scientific discovery.
-His work has been recognized with the Blackwell-Rosenbluth Award from the International Society for Bayesian Analysis.
+His work has been recognized with the Blackwell-Rosenbluth Award and an AISTATS Best Student Paper Award.
 Additionally, Geoff has co-founded several widely-used open source software projects, including the GPyTorch, LinearOperator, and CoLA libraries.
 `
